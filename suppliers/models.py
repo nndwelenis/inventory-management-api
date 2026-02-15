d7dcd3bcb7d787145a88754becadb32926f99b2c
nndwelenis <nndwelenis@gmail.com>
2026-02-15 11:35:38 +0200

    Add Supplier model and apply migration

--- suppliers/models.py
+++ suppliers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    contact_email = models.EmailField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
