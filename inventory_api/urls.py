13da122dc545e82edcfbafc2377c88f8c1e49cbf
nndwelenis <nndwelenis@gmail.com>
2026-02-19 18:10:34 +0200

    Implement Product API with ownership enforcement

--- inventory_api/urls.py
+++ inventory_api/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/suppliers/', include('suppliers.urls')),
+    path('api/products/', include('products.urls')),
 ]
