1716b3eb6b768fc2dd70f692f43a2ecebfff466f
nndwelenis <nndwelenis@gmail.com>
2026-02-20 17:57:09 +0200

    Add automated tests for product ownership and stock logic

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from .models import Product
 
 # Create your tests here.
+
+# First target:
+# 👉 Test Product ownership isolation
+
+# Meaning:
+# User A cannot see User B products.
+# User A only sees their own.
+
+class ProductOwnershipTest(APITestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create_user(username="jay", password="pass123")
+        self.user2 = User.objects.create_user(username="thabo", password="pass123")
+
+        self.product1 = Product.objects.create(
+            name="Coke",
+            description="Drink",
+            category="Beverage",
+            quantity_in_stock=10,
+            price=20,
+            owner=self.user1
+        )
+
+        self.product2 = Product.objects.create(
+            name="Bread",
+            description="Food",
+            category="Bakery",
+            quantity_in_stock=15,
+            price=15,
+            owner=self.user2
+        )
+
+    def test_user_sees_only_own_products(self):
+        self.client.login(username="jay", password="pass123")
+
+        url = reverse('product-list-create')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['name'], "Coke")
--- stock/tests.py
+++ stock/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from products.models import Product
+from .models import StockMovement
+
+# Now we test the real heart of the system:
+
+# 👉 Stock logic
+# This is more important than CRUD.
 
 # Create your tests here.
+class StockLogicTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="jay", password="pass123")
+
+        self.product = Product.objects.create(
+            name="Coke",
+            description="Drink",
+            category="Beverage",
+            quantity_in_stock=5,
+            price=20,
+            owner=self.user
+        )
+
+        self.client.login(username="jay", password="pass123")
+
+    def test_cannot_remove_more_than_available_stock(self):
+        url = reverse('stock-movement-create')
+
+        data = {
+            "product": self.product.id,
+            "movement_type": "OUT",
+            "quantity": 10
+        }
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Product.objects.get(id=self.product.id).quantity_in_stock, 5)
