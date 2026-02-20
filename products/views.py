2f2c2dcc5e81e1a1f35f262d5b3d3c8e1895ddca
nndwelenis <nndwelenis@gmail.com>
2026-02-20 18:24:04 +0200

    Add low stock filter to Product API

--- products/views.py
+++ products/views.py
@@ -19,12 +19,17 @@ class ProductListCreateView(generics.ListCreateAPIView):
     ordering_fields = ['price', 'quantity_in_stock', 'created_at']
 
     def get_queryset(self):
-        return Product.objects.filter(owner=self.request.user)
+        queryset = Product.objects.filter(owner=self.request.user)
+
+        low_stock = self.request.query_params.get('low_stock')
+
+        if low_stock == 'true':
+            queryset = queryset.filter(quantity_in_stock__lte=5)
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
